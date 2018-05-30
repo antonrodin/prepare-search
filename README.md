c0db1c1f3e9184dce0dffa0ccc16d256ecbb9ecf
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2018-05-30 10:19:01 +0200

    Escape special chars with some kind off white list

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Prepare String for Search
 
+1. The first thing to do is to remove special characters like ., :, ? or some others to whitespace, like so:
+
+```javascript
+```
+
+2. Before apply algoritm like mepaphone the string should be escaped and the special chars like ñ, Ñ, ç, Ç, á, Á, è or È converted to equivalent characters. like n, c, a...
+
 TODO
 
 * Escape and remove special chars like . ? : or ()
+* The Spanish Stop Words and strings have their "particular" letters
 * Remove the stop words in spanish, like el, esta, actual, mas
 * Convert string with Metaphone algorytm.
--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
+// Array of Spanish Stop Words
 const stopwords = require('stopwords-es');
+
+// Metaphone function
 const metaphone = require('metaphone');
 
-const testString = "Este es un ejemplo de cadena de texto, de la cual eliminamos los Stop Words en Español y caractres espaciales como ?, ! o ()";
+let str = "Este es un ejemplo de cadena de texto, de la cual eliminamos los Stop Words en Español ÉSTE y caractres espaciales mèdic çaça como por este: ?, ! o ()";
 
-// Stop Words Array
-const swArr = stopwords;
+// Remove all special chars. [^\w] - Match anything except alphanumeric characters and special spanish letters like ñ
+str = str.replace(/[^\w\sñÑçÇáéíóúÁÉÍÓÚüÜàèìòùÀÈÌÒÙ]/gi, "");
+console.log(str);
 
-String.prototype.escapeSpecialChars = function() {
-    let replaceString = this;
-    return replaceString.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "");
-}
 
-// Extends string to replace the stop array with nothing
-String.prototype.replaceStopWords = function(find) {
-    let replaceString = this;
-    let re;
-    for (let i = 0; i < find.length; i++) {
-        re = new RegExp(`${find[i]}`, "gi");
-        console.log(re);
-        //replaceString = replaceString.replace(re, "");
-    }
-    return replaceString;
-  };
 
-console.log(testString);
-console.log(testString.escapeSpecialChars());
+// String.prototype.escapeSpecialChars = function() {
+//     let replaceString = this;
+//     return replaceString.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "");
+// }
+
+// // Extends string to replace the stop array with nothing
+// String.prototype.replaceStopWords = function(find) {
+//     let replaceString = this;
+//     let re;
+//     for (let i = 0; i < find.length; i++) {
+//         re = new RegExp(`^${find[i]}$`, "gi");
+//         //console.log(re);
+//         replaceString = replaceString.replace(re, "");
+//     }
+//     return replaceString;
+//   };
